7bbe13576c900030b54dba6f5aab7b52778957f4
readberg <wayreadberg@gmail.com>
2023-02-19 15:54:50 +0300

    refactor: countdown

---
+++ js/countdown.js
@@ -0,0 +1,58 @@
+let days = document.querySelector('.days'),
+    hours = document.querySelector('.hours'),
+    minutes = document.querySelector('.minutes'),
+    seconds = document.querySelector('.seconds');
+
+let textDays = document.querySelector('.days-text'),
+    textHours = document.querySelector('.hours-text'),
+    textMinutes = document.querySelector('.minutes-text'),
+    textSeconds = document.querySelector('.seconds-text');
+
+
+// Date to which the countdown goes
+const finalTime = new Date(`May 31, 2023 00:00:00`);
+
+function updateCounter() {
+    const currentTime = new Date();
+
+    const diff = finalTime - currentTime;
+
+    // Transfer in days
+    const daysLeft = Math.floor(diff / 1000 / 60 / 60 / 24);
+
+    // Translation to hours
+    const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
+
+    // Translation into minutes
+    const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
+
+    // Conversion to seconds
+    const secondsLeft = Math.floor(diff / 1000) % 60;
+
+    // Adding data to the page
+    days.innerText = daysLeft < 10 ? '0' + daysLeft : daysLeft;
+    hours.innerText = hoursLeft < 10 ? '0' + hoursLeft : hoursLeft;
+    minutes.innerText = minutesLeft < 10 ? '0' + minutesLeft : minutesLeft;
+    seconds.innerText = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
+
+    editCountdownText();
+}
+updateCounter();
+
+// Running the function every second
+setInterval(updateCounter, 1000);
+
+// Change the text in the countdown when adapting
+function editCountdownText() {
+    if (window.innerWidth <= 768) {
+        textDays.innerText = 'DD';
+        textHours.innerText = 'HH';
+        textMinutes.innerText = 'MM';
+        textSeconds.innerText = 'SS';
+    } else {
+        textDays.innerText = 'Days';
+        textHours.innerText = 'Hours';
+        textMinutes.innerText = 'Minutes';
+        textSeconds.innerText = 'Seconds';
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict"
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('form');
     form.addEventListener('submit', formSend);
 
@@ -69,75 +69,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Close Popup
-    $('.close-popup').click(function () { // by clicking on the cross
+    $('.close-popup').click(() => { // by clicking on the cross
         $('.js-overlay').fadeOut();
     });
-    $('.close-btn').click(function () { // by clicking on the cross
+    $('.close-btn').click(() => { // by clicking on the cross
         $('.js-overlay').fadeOut();
     });
 
-    $(document).mouseup(function (e) { // by clicking outside the popup
+    $(document).mouseup((e) => { // by clicking outside the popup
         const popup = $('.popup');
         if (e.target != popup[0] && popup.has(e.target).length === 0) {
             $('.js-overlay').fadeOut();
         }
     });
-
-
-    // Timer
-
-    const days = document.querySelector('.days');
-    const hours = document.querySelector('.hours');
-    const minutes = document.querySelector('.minutes');
-    const seconds = document.querySelector('.seconds');
-
-    const finalTime = new Date(`May 31, 2023 00:00:00`);
-    function updateCounter() {
-        const currentTime = new Date();
-
-        const diff = finalTime - currentTime;
-
-        // Transfer in days
-        const daysLeft = Math.floor(diff / 1000 / 60 / 60 / 24);
-
-        // Translation to hours
-        const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
-
-        // Translation into minutes
-        const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
-
-        // Conversion to seconds
-        const secondsLeft = Math.floor(diff / 1000) % 60;
-
-        // Adding data to the page
-        days.innerText = daysLeft < 10 ? '0' + daysLeft : daysLeft;
-        hours.innerText = hoursLeft < 10 ? '0' + hoursLeft : hoursLeft;
-        minutes.innerText = minutesLeft < 10 ? '0' + minutesLeft : minutesLeft;
-        seconds.innerText = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
-
-        const textDays = document.querySelector('.days-text');
-        const textHours = document.querySelector('.hours-text');
-        const textMinutes = document.querySelector('.minutes-text');
-        const textSeconds = document.querySelector('.seconds-text');
-
-        if (window.innerWidth <= 768) {
-            textDays.innerText = 'DD';
-            textHours.innerText = 'HH';
-            textMinutes.innerText = 'MM';
-            textSeconds.innerText = 'SS';
-        } else {
-            textDays.innerText = 'Days';
-            textHours.innerText = 'Hours';
-            textMinutes.innerText = 'Minutes';
-            textSeconds.innerText = 'Seconds';
-        }
-    }
-
-    updateCounter();
-
-    // Running the function every second
-    setInterval(updateCounter, 1000);
-
 })
 
 
